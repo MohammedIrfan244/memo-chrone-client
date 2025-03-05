987c4348d1638e81f98d237b09b3d4c4856d2a9a
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-03-05 17:14:20 +0530

    feat: add axios instance with request interceptor and error handling utility

---
+++ src/lib/utils/axiosInstance.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import handleAxiosError from "./handleAxiosError";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/auth/refresh`,
+          { withCredentials: true }
+        );
+        localStorage.setItem("accessToken", response.data.accessToken);
+        const accessToken = response.data.accessToken;
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${accessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        console.log(handleAxiosError(error));
+        const existingRefreshToken = localStorage.getItem("refreshToken");
+        if (existingRefreshToken) {
+          await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/logout`, {
+            withCredentials: true,
+          });
+        }
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("user");
+        return Promise.reject(error);
+      }
+    }
+    console.log(handleAxiosError(error));
+    return Promise.reject(error);
+  }
+);
